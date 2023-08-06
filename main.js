0294f7b7bc5b43fbe3ed99044683a4b2088b9459
cokingtins1 <106932061+cokingtins1@users.noreply.github.com>
2023-08-05 21:48:16 -0400

    functioning app connected to api

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 import "./style.css"
+import { getWeather } from "./weather"
+
+getWeather(39.3321, 84.4173, Intl.DateTimeFormat().resolvedOptions().timeZone).then(renderWeather).catch(e =>{
+    console.error(e)
+    alert('error getting weather')
+})
+
+function renderWeather({current}){
+    renderCurrentWeather(current)
+}
+
+// Helper function for renderCurrentWeather()
+function setValue(selector, value, unit, {parent = document} = {}){
+    parent.querySelector(`[data-${selector}]`).textContent = value + unit
+}
+
+function renderCurrentWeather(current){
+    setValue('current-temp', current.currentTemp,'°')
+    setValue('high', current.highTemp, '°')
+    setValue('low', current.lowTemp, '°')
+    setValue('wind', current.windSpeed, ' mph')
+
+}
---
+++ weather.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+
+export function getWeather(lat, lon, timezone) {
+	return axios.get("https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime", {
+		params: {
+			latitude: lat,
+			longitude: lon,
+			timezone: timezone,
+		},
+	}).then(({data}) => {
+        return {
+            current: parseCurrentWeather(data),
+            // daily: parseDailyWeather(data)
+        }
+    });
+}
+
+function parseCurrentWeather({current_weather, daily}){
+    const {
+        temperature: currentTemp, 
+        windspeed: windSpeed, 
+        weathercode:iconCode
+    } = current_weather
+
+    const {
+        temperature_2m_max: [maxTemp],
+        temperature_2m_min: [minTemp],
+    } = daily
+
+    return{
+        currentTemp: Math.round(currentTemp),
+        highTemp: Math.round(maxTemp),
+        lowTemp: Math.round(minTemp),
+        windSpeed: Math.round(windSpeed),
+        iconCode,
+    }
+}
+
+
